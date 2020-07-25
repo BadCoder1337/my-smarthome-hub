47f91971432407b2a50fd001affaca89260e3985
BadCoder1337 <anton.tiz.musin@gmail.com>
2020-07-25 22:11:00 +0300

    Dev mode

--- src/index.ts
+++ src/index.ts
@@ -31,14 +31,22 @@ async function getDevices() {
 }
 
 async function main() {
+    const isDevMode = process.env.NODE_ENV === 'development';
+
     const [cfg, codeMap] = await loadCodes();
     console.log('[1/4] Codes loaded!');
+    isDevMode && console.log(cfg, codeMap);
+
     const arpTable = await getArpTable();
     console.log('[2/4] ARP table loaded!', JSON.stringify(arpTable), process.env.IP, process.env.MASK);
+
     const devicesCache = await getDevices();
     console.log('[3/4] Cached devices loaded!');
+    isDevMode && console.log(devicesCache);
+    
     const ewe = new eWeLink({arpTable, devicesCache});
     console.log('[4/4] Ready!');
+    isDevMode && console.log(ewe);
 
     await mqtt.subscribe('tele/tasmota/#');
     mqtt.on('message', async (topic: string, payload: Buffer) => {
